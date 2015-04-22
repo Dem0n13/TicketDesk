2ab0cd5cdc84772df40098c5950fad7e4cf09de0
Dem0n13 <Dem0n13,kaluga@gmail.com>
2015-04-22 16:31:54 +0300

    added api/categories, api/priorities, api/types

---
+++ TicketDesk-2.5/TicketDesk.Web.Client/Api/Controllers/StaticEntitiesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using TicketDesk.Domain;
+using TicketDesk.Domain.Model;
+
+namespace TicketDesk.Web.Client.Api.Controllers {
+    [RoutePrefix("api")]
+    [Authorize(Roles = "TdInternalUsers")]
+    public class StaticEntitiesController : ApiController {
+        private static readonly object Statuses = Enum.GetValues(typeof(TicketStatus))
+            .Cast<TicketStatus>()
+            .Select(status => new {Value = status, Title = status.GetDescription()})
+            .ToArray();
+        private static readonly ApplicationSelectListSetting DefaultListSettings = new ApplicationSelectListSetting();
+
+        // GET: api/statuses
+        [HttpGet]
+        [Route("statuses")]
+        public IHttpActionResult GetStatuses() {
+            return Ok(Statuses);
+        }
+
+        // GET: api/categories
+        [HttpGet]
+        [Route("categories")]
+        public IHttpActionResult GetCategories() {
+            return Ok(DefaultListSettings.CategoryList);
+        }
+
+        // GET: api/priorities
+        [HttpGet]
+        [Route("priorities")]
+        public IHttpActionResult GetPriorities() {
+            return Ok(DefaultListSettings.PriorityList);
+        }
+
+        // GET: api/types
+        [HttpGet]
+        [Route("types")]
+        public IHttpActionResult GetTypes() {
+            return Ok(DefaultListSettings.TicketTypesList);
+        }
+    }
+}
